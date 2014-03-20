54a3c2b317e272494afeb1349ab84ce471b9db38
Brett Campbell <invsblduck@gmail.com>
2014-03-20 13:19:55 -0700

    make keys fasterrrrrrr

--- fixshit.sh
+++ fixshit.sh
@@ -2,6 +2,6 @@
 #echo 0 |sudo tee /sys/class/backlight/intel_backlight/brightness
 setxkbmap dvorak
 xmodmap /home/duck/.xmodmaprc
-xset r rate 180 100
+xset r rate 170 150
 #xrandr --output DP1 --primary --mode 1920x1080 --output LVDS1 --mode 1366x768 --below DP1
 sudo iwconfig wlp4s0 power off
