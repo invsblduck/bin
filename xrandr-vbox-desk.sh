315bdc802d379b4da8c1a40783d5c87c18ed2202
Brett Campbell <invsblduck@gmail.com>
2014-08-23 00:26:09 -0700

    use simple xrandr command for standard screen orientation
    one monitor rotated 90 degrees was confusing all the softwarez

--- xrandr-vbox-desk.sh
+++ xrandr-vbox-desk.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
-xrandr --output VBOX0 --mode 1920x1015 --primary \
-       --output VBOX1 --mode 1080x1855 --right-of VBOX0
+
+##
+# Right monitor rotated 90 degrees (PITA)
+#xrandr --output VBOX0 --mode 1920x1015 --pos 0x420 --primary \
+#       --output VBOX1 --mode 1080x1855 --pos 1920x0
+
+##
+# Both monitors in standard orientation
+xrandr --output VBOX0 --auto --primary \
+       --output VBOX1 --auto --right-of VBOX0
