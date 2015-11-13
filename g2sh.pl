bc89fbc5de780b5b93f3681eb61845861944b610
Brett Campbell <invsblduck@gmail.com>
2015-11-12 23:27:54 -0800

    Revive g2sh :)

--- g2sh.pl
+++ g2sh.pl
@@ -21,45 +21,45 @@ $helper = ($@) ? 0 : 1;
 #}
 
 # fortune is my friend
-#system ("which fortune >/dev/null 2>&1");
-#
-#if ($? >> 8 == 0) {
-#
-#    $cmd = "fortune -n 150 -s";
-#
-#    system ("which cowsay >/dev/null 2>&1");
-#    $cmd .= " |cowsay -f tux" unless ($? >> 8 != 0);
-#
-#    system ("$cmd > ~/.motd");
-#}
-#else {
-#
-#    open(MOTD, "> $ENV{'HOME'}/.motd");
-#    print MOTD << 'EOF';
-#             ,        ,
-#            /(        )`
-#            \ \___   / |
-#            /- _  `-/  '
-#           (/\/ \ \   /\    FreeBSD!
-#           / /   | `    \
-#           O O   ) /    |
-#           `-^--'`<     '
-#          (_.)  _  )   /
-#           `.___/`    /
-#             `-----' /
-#<----.     __ / __   \
-#<----|====O)))==) \) /====
-#<----'    `--' `.__,' \
-#             |        |
-#              \       /
-#        ______( (_  / \______
-#      ,'  ,-----'   |        \
-#      `--{__________)        \/
-#
-#EOF
-#
-#    close MOTD;
-#}
+system ("which fortune >/dev/null 2>&1");
+
+if ($? >> 8 == 0) {
+
+    $cmd = "fortune -n 150 -s";
+
+    system ("which cowsay >/dev/null 2>&1");
+    $cmd .= " |cowsay -f tux" unless ($? >> 8 != 0);
+
+    system ("$cmd > ~/.motd");
+}
+else {
+
+    open(MOTD, "> $ENV{'HOME'}/.motd");
+    print MOTD << 'EOF';
+             ,        ,
+            /(        )`
+            \ \___   / |
+            /- _  `-/  '
+           (/\/ \ \   /\    FreeBSD!
+           / /   | `    \
+           O O   ) /    |
+           `-^--'`<     '
+          (_.)  _  )   /
+           `.___/`    /
+             `-----' /
+<----.     __ / __   \
+<----|====O)))==) \) /====
+<----'    `--' `.__,' \
+             |        |
+              \       /
+        ______( (_  / \______
+      ,'  ,-----'   |        \
+      `--{__________)        \/
+
+EOF
+
+    close MOTD;
+}
 
 # remove host from ~/.ssh/known_hosts
 Ops::Helper::cleanse_known_hosts_file($host) if ($helper);
@@ -88,14 +88,25 @@ if ($ping) {
     print "OK!\n";
 }
 
-# scp shiz to $host
-#if ($host !~ /^sandwichmaker(\.corp)?(\.expertcity\.com)?$/) {
-#
-#    @files = qw( ~/g2sh.pl ~/.vimrc ~/.toprc ~/.bash_profile
-#                 ~/.bashrc ~/.bash_aliases ~/.inputrc ~/.motd ~/.motd.old);
-#
-#    system ("/usr/bin/scp -r @files $host:");
-#}
+# find out which host we're on
+chomp($current_host = `hostname`);
+
+# scp shiz to target $host
+if ($current_host == 'archie') {
+
+    $dotfiles = '/git/invsblduck/fakecloud_configs/dotfiles';
+    foreach (qw(.vim* .toprc .bash* .inputrc .dircolors)) {
+        push @files, "$dotfiles/$_";
+    }
+    push @files, ('/git/invsblduck/bin/g2sh.pl', '~/.motd');
+}
+else {
+
+    @files = qw( ~/g2sh.pl ~/.vimrc ~/.vim ~/.toprc ~/.bash_profile
+                ~/.bashrc ~/.bash ~/.inputrc ~/.dircolors ~/.motd );
+}
+system ("rsync -aLk -i --info=flist1,stats0" .
+        " --exclude='**/*.swp' --exclude='**/*.un~' @files $host:");
 
 # ssh your bad self there ...
 exec ("ssh $host") or die "could not exec(): $!\n";
