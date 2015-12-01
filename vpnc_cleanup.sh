745d6ef4ed30794a0a776e69f19829e47b6da944
Brett Campbell <invsblduck@gmail.com>
2015-11-30 16:03:03 -0800

    add eth0 to vpn_cleanup.sh

--- vpnc_cleanup.sh
+++ vpnc_cleanup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 if ! pgrep vpnc >/dev/null; then
-    for iface in tun0 enp0s3; do
+    for iface in tun0 enp0s3 eth0; do
         if [ -e /run/resolvconf/interfaces/$iface ]; then
             sudo resolvconf -d $iface
         fi
