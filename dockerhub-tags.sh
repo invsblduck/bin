4678c3f0a3e0bd0b18f43ac78f65f34a8203e7a4
Brett Campbell <invsblduck@gmail.com>
2020-09-27 11:39:26 -0700

    write script for revealing dockerhub image tags

---
+++ dockerhub-tags.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+PROG_NAME="${0##*/}"
+HUB_URL=https://registry.hub.docker.com/v1/repositories
+
+if [ -z "$1" ] || [[ $1 =~ ^--?h(elp)? ]]; then
+    echo "usage: ${PROG_NAME} <IMAGE>"
+    echo
+    echo "List all tags for <IMAGE> on DockerHub"
+    exit 1
+fi
+
+image="$1"
+echo "fetching tags for image \`${image}'..."
+
+curl -sS "${HUB_URL}/${image}/tags" \
+    | sed -e 's/[][]//g' -e s/\"//g -e 's/ //g' \
+    | tr '}' '\n'  \
+    | awk -F: '{print $3}'
