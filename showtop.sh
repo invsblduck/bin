4f9a08c3cc78866baf0ad93b081a42bf9f3f3ea1
Brett Campbell <invsblduck@gmail.com>
2021-01-12 22:49:14 -0800

    new battle station: scripts!

---
+++ showtop.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+xfce4-terminal -e bpytop \
+               --title "Resource Monitor" \
+               --icon /usr/share/icons/Adwaita/48x48/legacy/system-run.png \
+               --geometry 106x65 \
+               --hide-menubar \
+               --hide-toolbar \
+               --hide-scrollbar \
+
---
+++ vpn-launcher.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+xfce4-terminal -e /data/lmi/opsvpn/connect.sh \
+               --title "OpsVPN" \
+               --icon /usr/share/icons/Adwaita/48x48/legacy/network-vpn.png \
+               --hide-menubar \
+               --hide-toolbar \
+               --hold \
+
